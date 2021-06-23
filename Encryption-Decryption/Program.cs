af49e97aed157973b8822fd9455a8d04c06eb095
Oleh <olehbobeliak@gmail.com>
2021-06-23 21:33:28 +0300

    Added longer message to test the cypher.

--- Encryption-Decryption/Program.cs
+++ Encryption-Decryption/Program.cs
@@ -7,7 +7,7 @@ namespace Encryption_Decryption
         static void Main(string[] args)
         {
 
-            Cypher msg = new Cypher("Hello world");
+            Cypher msg = new Cypher("Hello world. How are you guys? Cause i'm great!");
 
             string encrypted = msg.Encrypt();
             Console.WriteLine($"Encrypted message: {encrypted}");
